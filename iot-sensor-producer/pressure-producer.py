3ca7e324a3a44053d22cb31a8e03f46ed4a905fb
Mattia <scaccia.mattia@gmail.com>
2021-07-08 10:58:27 +0200

    Added pressure sensor

---
+++ iot-sensor-producer/pressure-producer.py
@@ -0,0 +1,52 @@
+import sys
+import csv
+import json
+import time
+from kafka import KafkaProducer
+
+FILENAME_PRESSURE_FMT = './2020-{}-pressure.csv'
+TOPIC = 'pressure-sensor'
+
+
+def create_producer(hostname, port):
+    producerEndpointFmt = '{}:{}'
+    return KafkaProducer(bootstrap_servers=producerEndpointFmt.format(hostname,port))
+
+
+def send_data(producerInstance, topicName, data, month_number):
+    producerInstance.send(topicName, data.encode('UTF-8'))
+    print('sent a message')
+    print(data)
+    print(month_number)
+    print()
+    return
+
+
+def read_file_and_send_data(producer, month_number):
+    filename = FILENAME_PRESSURE_FMT.format(month_number)
+    with open(filename) as csvFile:
+        reader = csv.DictReader(csvFile)
+        for row in reader:
+            jsonRow = json.dumps(row)
+            send_data(producer, TOPIC, jsonRow, month_number)
+            time.sleep(0.5)
+
+    
+def main():
+    time.sleep(15)
+    
+    producer = create_producer('kafka', '9092')
+    print(producer)
+    print()
+    
+    read_file_and_send_data(producer, '06')
+    read_file_and_send_data(producer, '07')
+    read_file_and_send_data(producer, '08')
+    read_file_and_send_data(producer, '09')
+    read_file_and_send_data(producer, '10')
+    read_file_and_send_data(producer, '11')
+    read_file_and_send_data(producer, '12')
+
+if __name__ == "__main__":
+    main()
+ 

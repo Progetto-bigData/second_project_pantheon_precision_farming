edf00b916c29947983644e231855389dce4ca822
Mattia <scaccia.mattia@gmail.com>
2021-07-08 10:58:14 +0200

    Added humidity sensor

---
+++ iot-sensor-producer/humidity-producer.py
@@ -0,0 +1,52 @@
+import sys
+import csv
+import json
+import time
+from kafka import KafkaProducer
+
+FILENAME_HUMIDITY_AVERAGE_FMT = './2020-{}-humidity-average.csv'
+FILENAME_HUMIDITY_MAX_FMT = './2020-{}-humidity-max.csv'
+FILENAME_HUMIDITY_MIN_FMT = './2020-{}-humidity-min.csv'
+TOPIC = 'humidity-sensor'
+
+def create_producer(hostname, port):
+    producerEndpointFmt = '{}:{}'
+    return KafkaProducer(bootstrap_servers=producerEndpointFmt.format(hostname,port))
+
+def send_data(producerInstance, topicName, data, month_number, filename):
+    producerInstance.send(topicName, data.encode('UTF-8'))
+    print('sent a message')
+    print(data)
+    print(month_number)
+    print(filename)
+    print()
+    return
+
+def read_file_and_send_data(producer, filenames, month_number):
+    for filename in filenames:
+        filename = filename.format(month_number)
+        with open(filename) as csvFile:
+            reader = csv.DictReader(csvFile)
+            for row in reader:
+                jsonRow = json.dumps(row)
+                send_data(producer, TOPIC, jsonRow, month_number, filename)
+                time.sleep(0.5)
+    
+def main():
+    time.sleep(15)
+    
+    producer = create_producer('kafka', '9092')
+    print(producer)
+    print()
+    filenames = [FILENAME_HUMIDITY_AVERAGE_FMT, FILENAME_HUMIDITY_MAX_FMT, FILENAME_HUMIDITY_MIN_FMT]
+    
+    read_file_and_send_data(producer, filenames, '06')
+    read_file_and_send_data(producer, filenames, '07')
+    read_file_and_send_data(producer, filenames, '08')
+    read_file_and_send_data(producer, filenames, '09')
+    read_file_and_send_data(producer, filenames, '10')
+    read_file_and_send_data(producer, filenames, '11')
+    read_file_and_send_data(producer, filenames, '12')
+
+if __name__ == "__main__":
+    main()

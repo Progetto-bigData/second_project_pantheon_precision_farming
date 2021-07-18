21b8b52c04e3faa21822ca65acb421ccedfaf7db
Mattia <scaccia.mattia@gmail.com>
2021-07-18 08:49:17 +0200

    Added dummy producer and consumer using influxDB

--- iot-consumer/dummy-consumer.py
+++ iot-consumer/dummy-consumer.py
@@ -1,18 +1,42 @@
 from pyspark.sql import SparkSession
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
+from influxdb import InfluxDBClient
 import time
+import random
+
+def read_data(x):
+    return x
+
+
+def map_save_influx(x,client):
+    data=x[1]
+    seconds = random.randint(0,59)
+    json = [
+        {
+            "measurement": "test",
+            "time": "2020-06-05T13:35:" + str(seconds) + ".000Z",
+            "fields": {
+                "field_test": data,
+            }
+        }
+    ]
+    client.write_points(json)
+    return x
+    
 
 def main():
-    time.sleep(5)
+    time.sleep(30)
     spark = SparkSession.builder.appName("Dummy-consumer").getOrCreate()
     sc = spark.sparkContext
     ssc = StreamingContext(sc, 10)  # batch interval to collect data
     
     kvs = KafkaUtils.createDirectStream(ssc, ["test"], {"metadata.broker.list": "kafka:9092"})
-    lines = kvs.map(lambda line: line)
-    #lines.pprint()
-    lines.saveAsTextFiles("/datalake/")
+    client = InfluxDBClient(host='influx', port=8086, username='root', password='password')
+    client.switch_database('pantheon')
+    lines = kvs.map(lambda x: read_data(x)).map(lambda x : map_save_influx(x,client))
+    lines.pprint()
+    #lines.saveAsTextFiles("/datalake/")
     ssc.start()
     ssc.awaitTermination()
 
